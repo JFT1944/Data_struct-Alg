1ac9787b9f5792ccf9e8dea047262d2f75e9e2af
JFT1944 <96144063+JFT1944@users.noreply.github.com>
2023-05-30 20:14:42 -0400

    recursion addition

--- dsa-recursion/recursion.js
+++ dsa-recursion/recursion.js
@@ -1,72 +1,76 @@
 /** product: calculate the product of an array of numbers. */
-let total = 1
 function product(nums) {
-  // let total = 1
-  {console.log({total:total})}
-  total = total * nums[0]
-  nums.shift()  
-console.log(total, nums)
+
+if(!nums[0]) return 1
+let num = nums[nums.length-1]
+nums.pop()
+return num * product(nums)
 
 
-  if(nums.length === 0) {
-    console.log(total)
-    return total}
-  product(nums)
 }
 
 /** longest: return the length of the longest word in an array of words. */
-let longestWord = ''
-console.log({lW: longestWord.length})
+
 function longest(words) {
-  if(!words[0]) return longestWord
-  console.log(words)
-  console.log(longestWord)
-if(longestWord.length < words[0].length){
-  console.log('worked')
-  longestWord = words[words.length -1]
-}
+
+let word = words[words.length-1]
 words.pop()
+if(!words[0]) return word.length
+let longestWord = longest(words)
+if(word.length > longestWord){
+  return word.length
+}else{
+  return longestWord
 
-
-
-
-longest(words)
-return longestWord
 }
-
+}
 /** everyOther: return a string with every other letter. */
-let eO = [] 
+
 function everyOther(str) {
 
-if(!str[0]) return eO
+let letter = str[str.length-1]
+str = str.slice(0, str.length-1)
+if(!str[0]) return letter
+let newStr = everyOther(str)
+if(str.length % 2 === 0){
+  return newStr + letter
+}else{
+  return newStr
 
-eO.push(str[str.length-1])
-console.log(eO)
-console.log(str)
-str.pop()
-str.pop()
-everyOther(str)
-return {f:eO}
-}
+}}
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str, rstr) {
-if(!str[0]) return
-
-if(str[str.length-1] !== rstr[rstr.length -1]){
-  return false
-}
-
-
-str.pop()
-rstr.pop()
-isPalindrome(str, rstr)
+  if(!rstr){
+    rstr = str.split('').reverse().join('')
+  }
+  if(str === rstr){
+    return true
+  }else{
+    return false
+  }
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
+
+function helper(x, y){
+  if(x.length === 0) {return -1}
+  }
+  if(x[x.length -1] == y){
+    console.log([x, x[x.length -1]])
+    return x.length -1
+  }
+  x.pop()
+  console.log([x, y])
+  return helper(x, y)
+
+}
+return helper(arr, val)
+
+
 
 }
 
@@ -74,11 +78,37 @@ function findIndex(arr, val) {
 
 function revString(str) {
 
+function helper(str){
+  if(!str[0]) return ''
+  let letter = str[str.length-1]
+  str = str.slice(0, str.length-1)
+  return letter + helper(str)
+
+}
+helper(str)
+return helper(str)
+
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
+
+  // given an object, return an array of all of the string values. use recursion
+  function helper(){
+    for(let key in obj){
+      if(typeof obj[key] === 'object'){
+        helper(obj[key])
+      }else if(typeof obj[key] === 'string'){
+        arr.push(obj[key])
+      } 
+
+      
+    }
+  }
+  let arr = []
+  helper()
+  return arr
 
 }
 
@@ -90,11 +120,9 @@ function binarySearch(arr, val) {
 }
 
 
-
 // product([1,2,3,4, 10])
 // console.log(longest(['one', 'two', 'three', 'seventeen']))
-console.log(everyOther(['one', 'two', 'three', 'seventeen']))
-console.log(isPalindrome('racecar', 'racecar'))
+
 
 
 module.exports = {
@@ -106,4 +134,4 @@ module.exports = {
   revString,
   gatherStrings,
   binarySearch
-};
+}
